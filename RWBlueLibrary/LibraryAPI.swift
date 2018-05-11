78492cc7a1138a2c700a43824a7ff387cc83b702
splab-mac27 <splab-mac27@satonomacbook-puro.local>
2018-05-11 18:05:53 +0900

    Implemented Facade pattern

--- RWBlueLibrary/LibraryAPI.swift
+++ RWBlueLibrary/LibraryAPI.swift
@@ -5,10 +5,14 @@
 //  Created by splab-mac27 on 2018/05/11.
 //  Copyright © 2018 Razeware LLC. All rights reserved.
 //
-
+//Facade implementation
 import Foundation
 
 final class LibraryAPI {
+  private let persistencyManager = PersistancyManager()
+  private let httpClient = HTTPClient()
+  private let isOnline = false
+  
   
   //gives other objects access to the singleton
   static let shared = LibraryAPI()
